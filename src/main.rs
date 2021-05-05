456e43aa8b5c2d75da37cb4b52f361db6ed2d60e
gjz010 <gjz010944@gmail.com>
2021-05-06 01:55:14 +0800

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,44 @@
 
 #[macro_use]
 extern crate rcore_user;
-extern crate rvm;
 extern crate alloc;
 extern crate core;
+extern crate rvm;
 mod rvm_io;
 
-fn read_file(path: &str, buf: &mut [u8])->Result<usize, i32>{
-    use rcore_user::syscall::*;
+fn read_file(path: &str, buf: &mut [u8]) -> Result<usize, i32> {
     use rcore_user::io::*;
+    use rcore_user::syscall::*;
     let fd = sys_open(path, O_RDONLY);
-    if fd<0{
+    if fd < 0 {
         return Err(fd);
     }
     let len = sys_read(fd as usize, buf.as_mut_ptr(), buf.len());
     sys_close(fd as usize);
-    if len<0{
+    if len < 0 {
         return Err(len);
     }
     return Ok(len as usize);
 }
-fn rvm_main()->rvm_io::Result<()>{
+fn rvm_main() -> rvm_io::Result<()> {
     println!("rust-rvm-vmm starting");
     let vm = rvm_io::RVM::new("/dev/rvm")?;
     let vm_image_path = "/vmm/rvloader.img";
-    let mem = vm.add_memory_region(0x80200000, 128*1024*1024)?;
+    let mem = vm.add_memory_region(0x80200000, 128 * 1024 * 1024)?;
     read_file(vm_image_path, mem.data).unwrap();
     let vcpu = vm.create_vcpu(0x80200000)?;
-    loop{
+    loop {
         let packet = vm.resume(vcpu)?;
-        match packet.kind{
-            rvm::RvmExitPacketKind::GuestEcall=>{
-                let ecall = unsafe {&packet.inner.ecall};
-                if ecall.eid==1{
+        match packet.kind {
+            rvm::RvmExitPacketKind::GuestEcall => {
+                let ecall = unsafe { &packet.inner.ecall };
+                if ecall.eid == 1 {
                     rcore_user::io::putc(ecall.arg0 as u8);
-                }else{
+                } else {
                     println!("Bad ecall eid={} fid={}. Ignore.", ecall.eid, ecall.fid);
                 }
             }
-            _=>{
+            _ => {
                 println!("Bad exit. Exit.");
                 break;
             }
@@ -51,9 +51,9 @@ fn rvm_main()->rvm_io::Result<()>{
 
 #[no_mangle]
 fn main() {
-    match rvm_main(){
-        Ok(())=>{}
-        Err(x)=>{
+    match rvm_main() {
+        Ok(()) => {}
+        Err(x) => {
             println!("Error in RVM: {:?}", x);
         }
     }
